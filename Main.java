b1de9543e1eadd9d11159e6b362682712fa86416
mykolamatsiakh <mmatsiakh@gmail.com>
2016-11-23 00:19:29 +0200

    Wrote hello word

---
+++ Main.java
@@ -0,0 +1,13 @@
+package com.devcolibri;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.print("Hello world");
+        n();
+    }
+
+
+    static void n() {
+        System.out.print("Not work");
+    }
+}
