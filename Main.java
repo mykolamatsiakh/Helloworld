58381f3e4784752430d47f62d2ada0a16bef98a1
mykolamatsiakh <mmatsiakh@gmail.com>
2016-11-24 02:01:19 +0200

    Task about variables

--- Main.java
+++ Main.java
@@ -1,14 +1,37 @@
 package com.devcolibri;
 
-public class Main {
-    public static void main(String[] args) {
-        System.out.print("Hello world");
-        n();
-    }
+public class Main{
+
+static byte b = 8;
+static short sh = 34;
+static int i= 248;
+static long l = 56;
+
+static float fl = 2.3f;
+static double d = 2.3;
+
+static boolean bool = true;
+static boolean bool2 = false;
+
+public static void main(String [] args)
+{
+    System.out.print("byte: " + b);
+    System.out.print("short: " + sh);
+    System.out.print("int: "  + i);
+    System.out.print("long: "  + l);
+
+    System.out.print("float: " + fl);
+    System.out.print("double: " + d);
+    System.out.print(bool);
+    System.out.print(bool2);
 
 
-    static void n() {
-        System.out.print("Not work");
-    }
+
+
 }
-//.z,xmc,.xzmc,
+
+
+
+
+}
+
