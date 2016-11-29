79e85a33abdb780e691ac314c372d82de36a5483
mykolamatsiakh <mmatsiakh@gmail.com>
2016-11-29 23:02:49 +0200

    13 lesson

--- Main.java
+++ Main.java
@@ -1,20 +1,14 @@
 
 public class Main {
 
-    static int b=5;
-    static int num2=8;
-    static  boolean bool =true;
-
-
    public static void main(String [] args){
-     if(num2==b && bool)
-     {
-         System.out.println("Wow");
+    String[] st = "Hello World".split(" ");
 
-      }
-
-
-
+    for(String s:st){
+        System.out.println(s);
+    }
    }
+
+
 }
 
