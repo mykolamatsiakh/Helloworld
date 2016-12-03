bd2a45ce07794fecea415d47c41e6e6b263370f8
mykolamatsiakh <mmatsiakh@gmail.com>
2016-12-03 21:52:31 +0200

    17 lesson

--- Main.java
+++ Main.java
@@ -1,18 +1,18 @@
 package com.devcolibri;
 
 public class Main {
-    static   String mas[]=new String[3];
+
+   static int man[] =new int [10];
     public static void main(String[] args) {
-        mas[0]="Hello";
-        mas[1]=" ";
-        mas[2]="Mykola!";
 
 
-       for (int i=0;i<mas.length;i++){
-           System.out.print(mas[i]);
+
+       for (int i=man.length-1;i>=0;i--){
+           man[i] = i + 1;
+           System.out.println(man[i]);
         }
 
-    }
 
     }
+}
 
